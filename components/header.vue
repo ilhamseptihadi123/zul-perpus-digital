7e6ceda23863543fd0f7dbcd37d43fe33541b010
ilhamseptihadi123 <160100796+ilhamseptihadi123@users.noreply.github.com>
2024-02-29 13:00:21 +0700

    Add files via upload

---
+++ components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row p-3 bg-primary text-white">
+        <div class="col-lg-1">
+            <img src="~/assets/img/LOGO-SMKN4.jpg" alt="logo SMKN 4" >
+        </div>
+
+        <div class="col-lg-11">
+            <h1>perpus Digital</h1>
+            <address>
+                SMKN 4 TASIKMALAYA
+                JL.DEPOK, SUKAMENAK, PURBARATU
+            </address>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.logo {
+    width: 100%;
+}
+</style>
---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+                <div class="my-3">
+                    <input type="search" class="from-control from-control-lg rounded-5" placeholder="fillter...">
+                </div>
+                <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td>#</td>
+                            <td>NAMA</td>
+                            <td>KEANGGOTAAN</td>
+                            <td>WAKTU</td>
+                            <td>KEPERLUAN</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1.</td>
+                            <td>ILHAM SH</td>
+                            <td>Guru</td>
+                            <td>224 februari 2024, 23.31.00</td>
+                            <td>Baca</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
